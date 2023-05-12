7eb017a4016feffbe4c4acc62688c95cce497e3e
Cory Sherb <git@corysherb.com>
2023-05-11 22:45:12 -0400

    feat: add model config

--- python/ch1.py
+++ python/ch1.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import regex as re
+import torch
 from textacy.preprocessing.replace import urls
+from transformers import BertConfig, BertTokenizer, BertForSequenceClassification
+
+# test PyTorch
+x = torch.rand(5, 3)
+print(x)
 
 def keep_rows(text):
   this_bool = True
@@ -16,6 +22,8 @@ def clean(text):
   # Mask URLs with _URL_
   return urls(text)
   
+# PREPARE TEXT
+
 # Read the paragraph tokens from the OIG final reports
 file = 'data/oig_tokens.csv'
 df = pd.read_csv(file)
@@ -26,4 +34,10 @@ contains_letters = df['text'].map(keep_rows)
 df = df[contains_letters]
 # Clean the text that was kept
 df['text'] = df['text'].map(clean)
+
+# CONFIG MODEL
+
+config = BertConfig.from_pretrained('bert-base-uncased', finetuningtask='binary')
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 
